72f85e5ba2a1ae58f785829446365fb4f1ee553d
Jaspreet Singh <40349101+JustFact@users.noreply.github.com>
2024-10-27 16:27:17 +0530

    Add custom validation for email field

---
+++ script.js
@@ -0,0 +1,23 @@
+const form = document.querySelector("form");
+const email = document.querySelector("#email");
+const emailError = document.querySelector("#email + .errorMessage");
+console.log(emailError);
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log(email.validity.valueMissing);
+});
+
+email.addEventListener("input", (e) => {
+  //   console.log(e.currentTarget);
+  if (e.currentTarget.validity.valueMissing) {
+    emailError.textContent = "Value missing.";
+    email.classList.add("error");
+  } else if (!e.currentTarget.validity.valid) {
+    emailError.textContent =
+      "Value should be a correct email: <userId>@<domain>";
+    email.classList.add("error");
+  } else {
+    emailError.textContent = "";
+    email.classList.remove("error");
+  }
+});
