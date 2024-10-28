a0637eda18eea26871bea16d8fc929f00e830baa
Jaspreet Singh <40349101+JustFact@users.noreply.github.com>
2024-10-28 07:25:07 +0530

    Add custom validation for Country and Postal field

--- script.js
+++ script.js
@@ -28,3 +28,51 @@ email.addEventListener("blur", () => {
     email.classList.add("error");
   }
 });
+
+/* ====================================================== */
+
+const country = document.querySelector("#countrySelect");
+const postalCode = document.querySelector("#postalcode");
+const postalError = document.querySelector("#postalcode + .errorMessage");
+
+function checkPostalCode() {
+  const constraints = {
+    ch: [
+      "^(CH-)?\\d{4}$",
+      "Swiss postal codes must have exactly 4 digits: e.g. CH-1950 or 1950",
+    ],
+    fr: [
+      "^(F-)?\\d{5}$",
+      "French postal codes must have exactly 5 digits: e.g. F-75012 or 75012",
+    ],
+    de: [
+      "^(D-)?\\d{5}$",
+      "German postal codes must have exactly 5 digits: e.g. D-12345 or 12345",
+    ],
+    nl: [
+      "^(NL-)?\\d{4}\\s*([A-RT-Z][A-Z]|S[BCE-RT-Z])$",
+      "Dutch postal codes must have exactly 4 digits, followed by 2 letters except SA, SD and SS",
+    ],
+  };
+  const constraint = new RegExp(constraints[country.value][0], "");
+
+  if (constraint.test(postalCode.value)) {
+    postalError.textContent = "";
+    postalCode.classList.remove("error");
+  } else {
+    postalError.textContent = constraints[country.value][1];
+    postalCode.classList.add("error");
+
+    //Adding pattern attribute to trigger correct :valid :invalid pseudo selector
+    postalCode.setAttribute("pattern", constraints[country.value][0]);
+  }
+}
+
+country.addEventListener("change", checkPostalCode);
+postalCode.addEventListener("input", checkPostalCode);
+postalCode.addEventListener("blur", () => {
+  if (postalCode.validity.valueMissing) {
+    postalError.textContent = "Value Missing";
+    postalCode.classList.add("error");
+  }
+});
